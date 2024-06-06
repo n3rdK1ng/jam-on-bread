ad993f00e1008846b5332ea455ab30e12eb8da36
Marco <marco.velan10@gmail.com>
2024-06-06 23:42:04 +0200

    chore: config changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 
 import { ThemeProvider } from '#/components/theme-provider'
-import { cn } from '#/lib/utils'
+import { cn } from '#/utils/misc'
 
 import './globals.css'
 
