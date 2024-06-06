29a75bba4944812c79bd096d0214ba131a269550
Marco <marco.velan10@gmail.com>
2024-06-06 23:45:35 +0200

    feat: types

---
+++ utils/types.ts
@@ -0,0 +1,26 @@
+export type Amount = {
+	unit: string
+	quantity: string
+}
+
+export type AddressInfo = {
+	address: string
+	amount: Amount[]
+	stake_address: string
+	type: string
+	script: boolean
+}
+
+export type AssetInfo = {
+	asset: string
+	policy_id: string
+	asset_name: string
+	fingerprint: string
+	quantity: string
+	initial_mint_tx_hash: string
+	mint_or_burn_count: number
+	onchain_metadata: any
+	onchain_metadata_standard: string
+	onchain_metadata_extra: null
+	metadata: null
+}
