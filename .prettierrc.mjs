4de61dcaccd017b0fd8c2ca82a64cf442f8b61e3
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-06 14:36:58 +0200

    chore: format (#1)

---
+++ .prettierrc.mjs
@@ -0,0 +1,36 @@
+/** @type {import("prettier").Options} */
+export default {
+	arrowParens: 'avoid',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: 'always',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleAttributePerLine: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['**/*.json'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+	importOrder: ['^#(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	plugins: [
+		'prettier-plugin-tailwindcss',
+		'@trivago/prettier-plugin-sort-imports',
+	],
+}
