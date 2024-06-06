9796da69e6d377b888c6cb11b0065b7a4d719f20
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-07 00:24:39 +0200

    fix: ui issues & responsivity (#9)

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -12,12 +12,12 @@ import {
 	DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
-export function ModeToggle() {
+export const ModeToggle = () => {
 	const { setTheme } = useTheme()
 
 	return (
 		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
+			<DropdownMenuTrigger asChild className="absolute right-6 top-6">
 				<Button variant="outline" size="icon">
 					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
